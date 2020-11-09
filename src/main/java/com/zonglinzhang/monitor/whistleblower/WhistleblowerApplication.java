fd62ad60603cf8d2639aaa7e53d9f6a07a4798a5
zhangzonglin1 <zhangzonglin@popmart.com>
2020-11-09 11:28:11 +0800

    首次提交

---
+++ src/main/java/com/zonglinzhang/monitor/whistleblower/WhistleblowerApplication.java
@@ -0,0 +1,13 @@
+package com.zonglinzhang.monitor.whistleblower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhistleblowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhistleblowerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zonglinzhang/monitor/whistleblower/WhistleblowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zonglinzhang.monitor.whistleblower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhistleblowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
